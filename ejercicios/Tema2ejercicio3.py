ea78c894d4cf172edc978b9b99142df515cbbafb
Ana Castellano <anacastellanorubio@gmail.com>
2026-04-21 20:41:30 +0200

    modificaciones

---
+++ ejercicios/Tema2ejercicio3.py
@@ -0,0 +1,9 @@
+frase= input("Introduce una frase: ")
+
+longitud = len(frase)
+frase_mayusculas = frase.upper()
+frase_minusculas = frase.lower()
+print(frase)
+print("Longitud ", longitud)
+print(frase_mayusculas)
+print(frase_minusculas)    
--- ejercicios/Tema2ejercio2.py
+++ ejercicios/Tema2ejercio2.py
@@ -1,11 +1,11 @@
 #Definir los siguientes datos
-Camiseta = 10 
-Sudadera = 20.5
-Gorra = 5.5 
+camiseta = 10 
+sudadera = 20.5
+gorra = 5.5 
 
-print("Camiseta: 10 euros")
-print("Sudadera: 20.5 euros")
-print("Gorra: 5.5 euros")
+print("camiseta: 10 euros")
+print("sudadera: 20.5 euros")
+print("gorra: 5.5 euros")
 print("Que cantidad de cada articulo?")
 
 #Preguntar la cantidad a usuario
@@ -17,7 +17,7 @@ cantidad_gorra = int (input ("¿Cuántas gorras quiere? "))
 total_camiseta = camiseta * cantidad_camiseta
 total_sudadera = sudadera * cantidad_sudadera
 total_gorras = gorra * cantidad_gorra
-Total Compra = total_camiseta + total_sudadera + total_gorra
+Total compra = total_camiseta + total_sudadera + total_gorra
 
 #Añadir 21% IVA al total de la Compra 
 Total_compra_IVA = Total_compra +((Total_compra * 21)/100)
