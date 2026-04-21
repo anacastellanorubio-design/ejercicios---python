739ec56a82dab3e8623e0f520ad03027cd409789
Ana Castellano <anacastellanorubio@gmail.com>
2026-04-21 19:13:57 +0200

    tema1ejercicio1.py

--- ejercicios/Tema1ejercicio1.py
+++ ejercicios/Tema1ejercicio1.py
@@ -1,1 +1,27 @@
-print("Hola mundo")
+#Cadena
+nombre = "Ana"
+#Numeros (int)
+edad = 44
+#Numeros con decimales (float)
+pi = 3,1416
+#Boleanos (True o False)
+gusta_python = True
+
+#Imprimmir variables
+print("Nombre", nombre)
+print("Edad", edad)
+print("Pi" pi)
+print("Me gusta python?", gusta_python)
+
+#Obtener valores introducidos por el usuario
+nombre pelicula py= input("Como se llama tu pelicula favorita?")
+
+director de la pelicula = input ("Quién fue el director de la pelicula?")
+
+año de lanzamiento = input ("Cual fue el año de lanzamiento?")
+
+género = input ("A que genero pertenece?")
+
+duración = input ("Cual es la duración de la película?")
+
+premios = input ("Ha recibido algún premio?")
