6afe32b328b0cb13c25650d28a1c40ed0ae2eaf5
Ana Castellano <anacastellanorubio@gmail.com>
2026-04-21 21:03:00 +0200

    Tema2ejercicio4.py

---
+++ ejercicios/Tema2ejercicio4.py
@@ -0,0 +1,13 @@
+#Introducir dirección correo electrónico
+
+correo_electronico = input("Cual es tu correo electronico?")
+
+longitud = len (correo electronico)
+
+correo_mayusculas = correo_electronico.upper
+correo_minisculas = correo_electronico.lower
+
+print = (Correo Electronico, correo electronico)
+print = (Longitud, longitud)
+print = (Mayusculas, mayusculas)
+print =(Minusculas, minusculas)
