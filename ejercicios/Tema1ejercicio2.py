9f8c556c52965c19a06c0feb6eeb3f4a9a43cc8b
Ana Castellano <anacastellanorubio@gmail.com>
2026-04-21 19:34:50 +0200

    tema1 ejercicio2

---
+++ ejercicios/Tema1ejercicio2.py
@@ -0,0 +1,18 @@
+#Pide a la usuaria que introduzca la siguiente información sobre su canción favorita.
+titulo = ("Lemon Tree")
+Artista = ("Fools Garden")
+Año de lanzamiento = ("1995")
+duración en segundos = (3,11)
+tiene videoclip = (True)
+
+nombre canción = input("Como se llama tu canción favorita?")
+
+artista = input ("Quién fue el cantante?")
+
+año de lanzamiento = input ("Cual fue el año de lanzamiento?")
+
+duración = input ("Cuantos segundos dura la canción")
+
+tiene video clip= input ("Tiene videoclip?")
+
+premios = input ("Ha recibido algún premio?")
