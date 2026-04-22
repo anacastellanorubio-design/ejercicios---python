4c6a05776b07d0073252cbfd746cc42102ab6b60
Ana Castellano <anacastellanorubio@gmail.com>
2026-04-22 10:33:33 +0200

    modificaciones

--- ejercicios/Tema2ejercicio3.py
+++ ejercicios/Tema2ejercicio3.py
@@ -2,7 +2,8 @@ frase = input ("Introduce una frase")
 
 longitud = len(frase)
 frase_mayusculas = frase.upper()
-frase_minusculas = frase.lower())
+frase_minusculas = frase.lower()
+
 print (frase)
 print("Longitud, longitud")
 print("frase_mayuscula")
