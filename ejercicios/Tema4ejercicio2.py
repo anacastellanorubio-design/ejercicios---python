979c7b5ef802cad40982d9e314e5744363ccf67d
Ana Castellano <anacastellanorubio@gmail.com>
2026-04-23 19:12:46 +0200

    Tema4ejercicio2.py

---
+++ ejercicios/Tema4ejercicio2.py
@@ -0,0 +1,10 @@
+numero_ganador = 4
+
+for i in range(1):
+    numero = int(input("Adivina un número entre 1 y 10: "))
+
+if numero == 4:
+        print("Premio")
+
+else:
+      print("Ohhhh, fallaste")
