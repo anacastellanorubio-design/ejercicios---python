895d074889f51d4e37fb1e29a7b0f8aad1532c5b
Ana Castellano <anacastellanorubio@gmail.com>
2026-04-23 18:56:28 +0200

    Tema4ejercicio1.py

---
+++ ejercicios/Tema4ejercio1.py
@@ -0,0 +1,23 @@
+color = input("Elige un color (Rojo, Verde, Azul, Amarillo, Morado): ")
+
+if color == "Rojo":
+    print("Rojo": pasión y energía )
+
+elif color == "Verde":
+    print("Verde":esperanza y crecimiento)  
+
+elif color == "Azul":
+    print("Azul: calma y serenidad")
+
+elif color == "Amarillo":
+    print("Amarillo: felicidad y optimismo")    
+
+elif color == "Morado":
+    print("Morado: sabiduría y creatividad")
+
+else: 
+    print("color no válido")
+
+      
+
+
