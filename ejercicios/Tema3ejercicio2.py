3a1b723dbe242608fa1ad4656ea25e01583fdddc
Ana Castellano <anacastellanorubio@gmail.com>
2026-04-23 18:24:01 +0200

    Tema3ejercicio2.py

---
+++ ejercicios/Tema3ejercicio2.py
@@ -0,0 +1,10 @@
+
+euros = float(input("Cantidad en euros: "))
+
+
+dolares = euros * 1.1
+libras = euros * 0.87
+
+print("\n--- CONVERSIÓN DE MONEDA ---")
+print(f"Dólares: {dolares:.2f} $")
+print(f"Libras: {libras:.2f} £")
